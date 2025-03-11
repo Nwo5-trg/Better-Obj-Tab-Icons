a1952a2dbc9842ebe4181e11365367ef903ca4ff
nwo5-trg <dogamations@gmail.com>
2025-03-11 13:07:46 -0400

    actually make code not bad

--- changelog.md
+++ changelog.md
@@ -1,28 +1,29 @@
-﻿# v1.1.3
- 
- * I FORGOT THE SLOPE TAB IM CRYING :sob:
+﻿
+# v2.0.0
+- **Added** rewrote the entire mod because the code was so bad
+- **Added** new formating for changelog :3
+- **Removed** node ids dependency
+
+# v1.1.3
+- **Fixed** I FORGOT THE SLOPE TAB IM CRYING :sob:
 
 # v1.1.2
- 
- * "fix issue causing potential crashes" - hiimjustin000
+- **Fixed** "fix issue causing potential crashes" - hiimjustin000
 
 # V1.1.1
 
- * Added node ids dependency 
+- **Added** node ids dependency 
 
 # v1.1.0
-
- * Fixed 3DL icon being bad on low screen sizes
- * Added alt texture for custom tab
- * Make the code slightly more not bad
- * Fixed mod.json (i think)
- * Added cool titles to settings
- * Update geode version
+- **Added** alt texture for custom tab
+- **Added** titles to settings
+- **Fixed** 3DL icon being bad on low screen sizes
+- **Fixed** made code slightly more not bad (bro you didnt add indentation)
+- **Fixed** mod.json formating (no i didnt)
+- **Fixed** bump version
 
 # v1.0.1
-
- * I don't even know honestly
+- **???** I don't even know honestly
 
 # v1.0.0
-
- * Initial release
+- Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -1,158 +1,43 @@
-#include "Geode/modify/Modify.hpp"
 #include <Geode/Geode.hpp>
-#include <Geode/modify/LevelEditorLayer.hpp>
 #include <Geode/modify/EditorUI.hpp>
-#include <Geode/cocos/textures/CCTexture2D.h>
 
 using namespace geode::prelude;
 
-class $modify(editui, EditorUI) 
-{
-	bool init(LevelEditorLayer* editorLayer) 
-	{	
+auto mod = Mod::get();
+
+class $modify(EditUI, EditorUI) {
+
+	bool init(LevelEditorLayer* editorLayer) {	
 		if (!EditorUI::init(editorLayer)) return false;
 
-// this code is so bad but i cant find another way to do it :sob:
-// wait wait im stupid what hold up i can slightly fix it to not use 15 million variables
-// github is so hard bro idk what im doing
-            auto buildtabs = this->getChildByID("build-tabs-menu");
+        std::vector<std::pair<std::string, std::string>> tabs { // have to make a pair literally just for 3d tab
+            {"block-tab", "block-tab"}, {"outline-tab", "outline-tab"}, {"slope-tab", "slope-tab"},
+            {"hazard-tab", "hazard-tab"}, {"3d-tab", "threed-tab"}, {"portal-tab", "portal-tab"},
+            {"monster-tab", "monster-tab"}, {"pixel-tab", "pixel-tab"}, {"collectible-tab", "collectible-tab"},
+            {"icon-tab", "icon-tab"}, {"deco-tab", "deco-tab"}, {"sawblade-tab", "sawblade-tab"},
+            {"trigger-tab", "trigger-tab"}, {"custom-tab", "custom-tab"}
+        };
 
-// function but not
-auto setupSprite = [&] (CCSprite* spr, cocos2d::CCSprite* tabsprc) {
-if (!spr || !tabsprc) return;
-tabsprc->addChild(spr);
-static_cast<cocos2d::CCNode*>(tabsprc->getChildren()->objectAtIndex(0))->setVisible(false);
-spr->setScale (0.55);
-spr->setPosition(cocos2d::CCPoint(16, 7.75));
-spr->setOpacity(150);
-};
+        auto setupSprite = [] (CCSprite* spr, CCSprite* tab) {
+            if (!spr || !tab) return;
+            tab->addChild(spr);
+            tab->getChildByType<CCSprite>(0)->setVisible(false);
+            spr->setScale (0.55);
+            spr->setPosition(ccp(16, 7.75));
+            spr->setOpacity(150);
+        };
 
-// Custom tab
-if (Mod::get()->getSettingValue<bool>("customtab-toggle"))
-{
-if (!Mod::get()->getSettingValue<bool>("customtab-alt"))
-{
-setupSprite(CCSprite::create("custom-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("custom-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("custom-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("custom-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-else
-{
-setupSprite(CCSprite::create("custom-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("custom-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("custom-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("custom-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-}
-// Trigger tab
-if (Mod::get()->getSettingValue<bool>("triggertab-toggle"))
-{
-setupSprite(CCSprite::create("trigger-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("trigger-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("trigger-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("trigger-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Sawblade tab
-if (Mod::get()->getSettingValue<bool>("sawbladetab-toggle"))
-{
-setupSprite(CCSprite::create("sawblade-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("sawblade-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("sawblade-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("sawblade-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// 3D tab
-if (Mod::get()->getSettingValue<bool>("threedtab-toggle"))
-{
-
-cocos2d::CCSprite* threedtabspr;
-cocos2d::CCSprite* threedtabspr2;
-
-    if (!Mod::get()->getSettingValue<bool>("threedtab-alt"))
-    {
-setupSprite(CCSprite::create("3d-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("3d-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("3d-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("3d-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-    }
-    else
-    {
-setupSprite(CCSprite::create("3d-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("3d-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("3d-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("3d-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-    }
-}
-
-// Monster tab
-if (Mod::get()->getSettingValue<bool>("monstertab-toggle"))
-{
-setupSprite(CCSprite::create("monster-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("monster-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("monster-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("monster-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Pixel tab
-if (Mod::get()->getSettingValue<bool>("pixeltab-toggle"))
-{
-if (!Mod::get()->getSettingValue<bool>("pixeltab-alt"))
-{
-setupSprite(CCSprite::create("pixel-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("pixel-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("pixel-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("pixel-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-else
-{
-setupSprite(CCSprite::create("pixel-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("pixel-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("pixel-tab-alt.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("pixel-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-}
-
-// Collectible tab
-if (Mod::get()->getSettingValue<bool>("collectibletab-toggle"))
-{
-setupSprite(CCSprite::create("collectible-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("collectible-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("collectible-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("collectible-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}    
-
-// Icon tab
-if (Mod::get()->getSettingValue<bool>("icontab-toggle"))
-{
-setupSprite(CCSprite::create("icon-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("icon-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("icon-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("icon-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Block tab
-if (Mod::get()->getSettingValue<bool>("blocktab-toggle"))
-{
-setupSprite(CCSprite::create("block-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("block-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("block-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("block-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Outline tab
-if (Mod::get()->getSettingValue<bool>("outlinetab-toggle"))
-{
-setupSprite(CCSprite::create("outline-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("outline-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("outline-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("outline-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Hazard tab
-if (Mod::get()->getSettingValue<bool>("hazardtab-toggle"))
-{
-setupSprite(CCSprite::create("hazard-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("hazard-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("hazard-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("hazard-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Portal tab
-if (Mod::get()->getSettingValue<bool>("portaltab-toggle"))
-{
-setupSprite(CCSprite::create("portal-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("portal-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("portal-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("portal-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Deco tab
-if (Mod::get()->getSettingValue<bool>("decotab-toggle"))
-{
-setupSprite(CCSprite::create("deco-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("deco-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("deco-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("deco-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-
-// Slope tab holy how did i forget this
-if (Mod::get()->getSettingValue<bool>("slopetab-toggle"))
-{
-setupSprite(CCSprite::create("slope-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("slope-tab")->getChildren()->objectAtIndex(0))->getChildren()->objectAtIndex(0)));
-setupSprite(CCSprite::create("slope-tab.png"_spr), static_cast<cocos2d::CCSprite*>(static_cast<cocos2d::CCSprite*>(buildtabs->getChildByID("slope-tab")->getChildren()->objectAtIndex(1))->getChildren()->objectAtIndex(0)));
-}
-            log::debug("something happened so you did it right");
-
+        for (int i = 0; i < m_tabsMenu->getChildren()->count(); i++) {
+            if (!mod->getSettingValue<bool>(std::string(tabs[i].second) + "-toggle")) continue;
+            if (auto tab = m_tabsMenu->getChildByType<CCNode>(i)) {
+                auto spr = CCSprite::create(mod->getSettingValue<bool>(std::string(tabs[i].second) + "-alt") ? 
+                ("nwo5.better_object_tab_icons/" + tabs[i].first + "-alt.png").c_str() : 
+                ("nwo5.better_object_tab_icons/" + tabs[i].first + ".png").c_str());
+                setupSprite(spr, tab->getChildByType<CCNode>(0)->getChildByType<CCSprite>(0));
+                setupSprite(spr, tab->getChildByType<CCNode>(1)->getChildByType<CCSprite>(0));
+            }
+        }
+        
 		return true;
 	}
 };
